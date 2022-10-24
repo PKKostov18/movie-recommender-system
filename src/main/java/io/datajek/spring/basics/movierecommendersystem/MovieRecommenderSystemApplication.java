ca422570a06ac312818dde2cbe11888b14481c14
PKKostov18 <PKKostov18@codingburgas.bg>
2022-10-24 11:51:10 +0300

    Commit 02-7

--- src/main/java/io/datajek/spring/basics/movierecommendersystem/MovieRecommenderSystemApplication.java
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/MovieRecommenderSystemApplication.java
@@ -1,13 +1,24 @@
 package io.datajek.spring.basics.movierecommendersystem;
 
+import io.datajek.spring.basics.movierecommendersystem.lesson1.RecommenderImplementation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class MovieRecommenderSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieRecommenderSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        RecommenderImplementation recommender = new RecommenderImplementation();
+        String[] results = recommender.recommendMovies("Finding Dory");
+
+        System.out.println(Arrays.toString(results));
+
+        SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+
+
+    }
 
 }
---
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson1/ContentBasedFilter.java
@@ -0,0 +1,8 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson1;
+
+public class ContentBasedFilter {
+
+    public String[] getRecommendations(String movie) {
+        return new String[] {"Happy feet", "Ice Age", "Shark Tale"};
+    }
+}
